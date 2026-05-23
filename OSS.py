df33aaef11e6d2a17f50acf6368be29fed2ac11d
Park Geonwoo <sunhaapa@naver.com>
2026-05-23 15:39:48 +0900

    Evaluation

--- OSS.py
+++ OSS.py
@@ -1,7 +1,7 @@
-from sklearn import datasets
+from sklearn import datasets, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-# 1. 데이터셋 유방암 불러오기
+# 1. 데이터셋 유방암 불러오기(데이터셋 로드)
 cancer = datasets.load_breast_cancer()
 
 # 데이터 정보 출력
@@ -25,3 +25,10 @@ print(X_test.shape)
 # 3. 머신러닝 모델 학습
 model = LogisticRegression(max_iter=5000)
 model.fit(X_train, y_train)
+
+
+# 4. 예측 및 정확도
+y_pred = model.predict(X_test)
+
+scores = metrics.accuracy_score(y_test, y_pred)
+print(scores)
